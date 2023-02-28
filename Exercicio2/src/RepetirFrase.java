1f46e83034c0efdb41c7253a699a412ab0f3b076
Augusto99MK <carlosaugustox6@gmail.com>
2023-02-27 20:39:17 -0400

    main

--- Exercicio2/src/RepetirFrase.java
+++ Exercicio2/src/RepetirFrase.java
@@ -4,7 +4,9 @@ public class RepetirFrase {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Qual o seu nome: ");
+		System.out.println("Digite uma saudação: ");
+		Scanner scanner2 = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
 		String nome = scanner.next();
 		System.out.println("Seja bem vindo " + nome +"!");
 	
